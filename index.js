fc92498639523d1b21d0da1719497579c1531814
Aidan Tomcy <aidantomcy@gmail.com>
2022-01-09 11:29:14 +0530

    Change BOT_TOKEN to TENOR_KEY in url variable

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ client.on("message", async (msg) => {
       if (tokens.length > 1) {
         keywords = tokens.slice(1, tokens.length).join(" ");
       }
-      let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.BOT_TOKEN}&contentfilter=high`;
+      let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&contentfilter=high`;
       let response = await fetch(url);
       let json = await response.json();
       let index = Math.floor(Math.random() * json.results.length);
