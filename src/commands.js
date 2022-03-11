aadf16915a7d3a828706d44f950973e2a7a9da07
Aidan Tomcy <aidantomcy@gmail.com>
2022-03-11 16:36:20 +0530

    Add commands.js to store commands

---
+++ src/commands.js
@@ -0,0 +1,56 @@
+const hello = (msg) => {
+  msg.channel.send("Hello!");
+};
+
+const whatAmI = (msg) => {
+  let whatami = [
+    "Potato Person 🥔",
+    "Chocolate Lover 💩",
+    "Funky Monkey 🐒",
+    "Vain Brain 🧠",
+    "Cosmic Cuttlefish 🦑",
+    "Groovy Gorilla 🦍",
+    "Perky Penguin 🐧",
+    "Swift Snail 🐌",
+    "Fancy Frog 🐸",
+    "Sassy Snake 🐍",
+    "Lazy Lion 🦁",
+    "Auspicious Alien 👽",
+    "Tame Tomato 🍅",
+    "Smelly Socks 🧦",
+    "Elegant Elephant 🐘",
+    "Dumb Dolphin 🐬",
+  ];
+  index = Math.floor(Math.random() * whatami.length);
+  msg.channel.send(whatami[index]);
+};
+
+const sendGIF = (msg, tokens) => {
+  let keywords = "dog";
+
+  if (tokens.length > 1) {
+    keywords = tokens.slice(1, tokens.length).join(" ");
+  }
+  let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.TENOR_KEY}&contentfilter=high`;
+  let response = await fetch(url);
+  let json = await response.json();
+  index = Math.floor(Math.random() * json.results.length);
+  msg.channel.send(json.results[index].url);
+  msg.channel.send(`GIF from Tenor ${keywords}`);
+};
+
+const help = (msg) => {
+  msg.channel.send(`
+$hello   - Say Hello
+$whatami - Tells what you are
+$gif     - Give a random GIF
+$help    - Send this help message
+`);
+};
+
+module.exports = {
+  hello,
+  whatAmI,
+  sendGIF,
+  help,
+};
